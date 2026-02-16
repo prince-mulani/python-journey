35b5aa8679393d8ea92d317cd6bf1f7abce0233b
Prince Mulani <princemulani7@gmail.com>
2026-02-16 21:53:09 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,9 @@
+print("hello AI engineer")
+print ("mulani prince")
+print("gandhinagar university")
+print("16 feb")
+print('''hello AI engineer 
+my name is mulani prince
+i study ingandhinagar university
+today is 16 feb
+  ''')
