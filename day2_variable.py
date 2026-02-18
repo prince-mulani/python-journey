f5e6dae69376729b740c3357bafd4ebb2dc89303
Prince Mulani <princemulani7@gmail.com>
2026-02-18 21:20:40 +0530

    day2-variables and data types prectice

---
+++ day2_variable.py
@@ -0,0 +1,17 @@
+age=19
+CGPA=9.25
+name="mulani prince"
+print("my name is",name,"my age is",age,"my CGPA",CGPA)
+
+a="25"          # for converting string to int
+d=int(a)
+print(type(d))
+
+b=25             # for converting int to string
+c=str(b)
+print(type(c))
+
+#sum of two number 
+p=int(input("enter the number 1 :"))     #for taking user input     
+r=int(input("enter the second number:"))
+print("sum of ",p,"+",r,"=",p+r)
