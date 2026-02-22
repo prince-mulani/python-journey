51c5a1b56dc265bd3ec2f8ebc3bdcadb3cf55fab
Prince Mulani <princemulani7@gmail.com>
2026-02-22 14:51:40 +0530

    Add files via upload
    day 3 complete

---
+++ day3_input_operators.py
@@ -0,0 +1,14 @@
+#simple calculatour
+
+a = int(input("enter the number 1:"))
+b = int(input("enter the number 2:"))
+print(a,"+",b"=",a+b)
+print(a,"-",b"=",a-b)
+print(a,"*",b"=",a*b)
+print(a,"/",b"=",a/b)
+
+#percentage calculator
+
+total=int(input("enter the total mark:"))
+earn= int(input("enter the give number:"))
+print("percentage =",earn*100/total)
